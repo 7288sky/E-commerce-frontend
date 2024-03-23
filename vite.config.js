95bffba6cfa14456a8968271001b31531caffb13
7288sky <123364227+7288sky@users.noreply.github.com>
2024-03-23 13:45:31 +0530

    setProxy

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api/':"http://localhost:5000",
-      '/uploads/':"http://localhost:5000"
+      '/api/':"https://ecommerce-backend-w4ry.onrender.com",
+      '/uploads/':"https://ecommerce-backend-w4ry.onrender.com"
     }
   }
 })
